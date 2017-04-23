615b54d07277a4752f49a56ce748c06ac36e6be6
Juan Jesus Gomez Noya <burus_86@hotmail.com>
2017-04-23 06:46:58 +0200

    2017-04-23: Arreglado la ruta a las peticiones HTTP de tipo DELETE

--- routes/web.php
+++ routes/web.php
@@ -21,35 +21,35 @@ $app->group(['prefix' => 'api/v1'], function($app)
 	$app->get('alumnos','AlumnoController@index');
 	$app->get('alumnos/{id}','AlumnoController@getAlumno');
 	$app->post('alumnos','AlumnoController@createAlumno');
-	$app->put('alumnos/{id}','AlumnoController@updateAlumno');
-	$app->delete('alumnos/{id}','AlumnoController@deleteAlumno');
+	$app->put('alumnos/{id}','AlumnoController@updateAlumno'); // $app->get('alumnos/{id}/update','AlumnoController@updateAlumno');
+	$app->get('alumnos/{id}/delete','AlumnoController@deleteAlumno'); // $app->delete('alumnos/{id}','AlumnoController@deleteAlumno');
 
 	/* PROFESORES */
 	$app->get('profesores','ProfesorController@index');
 	$app->get('profesores/{id}','ProfesorController@getProfesor');
 	$app->post('profesores','ProfesorController@createProfesor');
 	$app->put('profesores/{id}','ProfesorController@updateProfesor');
-	$app->delete('profesores/{id}','ProfesorController@deleteProfesor');
+	$app->get('profesores/{id}/delete','ProfesorController@deleteAlumno'); // $app->delete('profesores/{id}','ProfesorController@deleteProfesor');
 
 	/* ASIGNATURAS */
 	$app->get('asignaturas','AsignaturaController@index');
 	$app->get('asignaturas/{id}','AsignaturaController@getAsignatura');
 	$app->post('asignaturas','AsignaturaController@createAsignatura');
 	$app->put('asignaturas/{id}','AsignaturaController@updateAsignatura');
-	$app->delete('asignaturas/{id}','AsignaturaController@deleteAsignatura');
+	$app->get('asignaturas/{id}/delete','AsignaturaController@deleteAlumno'); // $app->delete('asignaturas/{id}','AsignaturaController@deleteAsignatura');
 
 	/* NOTAS */
 	$app->get('notas','NotaController@index');
 	$app->get('notas/{id}','NotaController@getNota');
 	$app->post('notas','NotaController@createNota');
 	$app->put('notas/{id}','NotaController@updateNota');
-	$app->delete('notas/{id}','NotaController@deleteNota');
+	$app->get('notas/{id}/delete','NotaController@deleteAlumno'); // $app->delete('notas/{id}','NotaController@deleteNota');
 
 	/* USUARIOS */
 	$app->get('usuarios','UsuarioController@index');
 	$app->get('usuarios/{id}','UsuarioController@getUsuario');
 	$app->post('usuarios','UsuarioController@createUsuario');
 	$app->put('usuarios/{id}','UsuarioController@updateUsuario');
-	$app->delete('usuarios/{id}','UsuarioController@deleteUsuario');
+	$app->get('usuarios/{id}/delete','UsuarioController@deleteAlumno'); // $app->delete('usuarios/{id}','UsuarioController@deleteUsuario');
 
 });
